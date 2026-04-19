02ed8346271440a039965b08cc753d4e5f7f1124
AnujAcharya2338 <anujacharya877@gmail.com>
2026-04-19 15:48:25 +0545

    learned about css types and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Card from "./card"
+
+import React from 'react';  
+import Student from "./Student";
+
 function App() {
  
  return(<>
-  <Card/>
-  <Card/>
+ <Student name="Luffy" age={20} isStudent={false} />
+ <Student name="Zoro" age={21} isStudent={false} />
+ <Student name="Nami" age={18} isStudent={true} />
+ <Student name="Ussop" age={22} isStudent={false} />
+ <Student name="Sanji"/>
   </>
  );
 
