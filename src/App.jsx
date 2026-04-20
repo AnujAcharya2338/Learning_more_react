f0a071e2f756984bf1cefb8e2e53193548467e09
AnujAcharya2338 <anujacharya877@gmail.com>
2026-04-20 13:09:27 +0545

    rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-
-import PropTypes from 'prop-types'
-
-import React from 'react';  
-import UserGreeting from './UserGreeting';
+import List from "../../completed/List";
+import React from "react";
 
 function App() {
- 
- return(<>
- <UserGreeting isLoggedIn = {true} username = "Anuj">
+  const fruits = [
+    { id: 1, name: "Apple", calories: 95 },
+    { id: 2, name: "Oranges", calories: 45 },
+    { id: 3, name: "Banana", calories: 105 },
+    { id: 4, name: "Coconut", calories: 159 },
+    { id: 5, name: "Pineapple", calories: 39 },
+  ]
 
- </UserGreeting>
+  const vegetables = [
+    { id: 6, name: "Potato", calories: 110 },
+    { id: 7, name: "Celery", calories: 34 },
+    { id: 8, name: "Carrots", calories: 87 },
+    { id: 9, name: "Corn", calories: 95 },
+    { id: 10, name: "Broccoli ", calories: 122 },
+  ]
 
- <UserGreeting isLoggedIn = {true}/>
-  </>
- );
-
-
-export default App
+  return(<> {fruits.length > 0  ? <List items={fruits} category="Fruits"></List>:null }
+  {vegetables.length > 0  ? <List items={vegetables} category="Vegetables"></List>:null}
+  </>);
+}
+export default App;
