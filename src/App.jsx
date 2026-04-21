91e2ac1e3d059b1550ca3d17ee516362ab9bd086
AnujAcharya2338 <anujacharya877@gmail.com>
2026-04-21 18:25:38 +0545

    onchange event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Counter from "../Counter";
+import MyComponent from "./MyComponent";
 function App() {
   
 
   return(<> 
-  <Counter></Counter>
+  <MyComponent></MyComponent>
   </>);
 }
 export default App;
