5ac74a58dabfca57c446ad1d0cc6c527e37680ef
AnujAcharya2338 <anujacharya877@gmail.com>
2026-04-21 21:37:06 +0545

    update arrays of objects in state i

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MyComponent2 from "./MyComponent2";
+import MyComponent3 from "./MyComponent3";
 function App() {
   
 
   return(<> 
-  <MyComponent2></MyComponent2>
+  <MyComponent3></MyComponent3>
   </>);
 }
 export default App;
